023f3fb0f4c9be4cf89f36ee5c0d774dd7dd165b
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-09-29 15:24:10 -0500

    Update backend.py
    A really basic test to see that flake8 does static analysis. We see if the tool catches "A"+"B" and suggests concatenation.

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -2,3 +2,11 @@
 # from deepface import DeepFace
 
 # TODO use the deepface library to do some stuff
+
+# here is a python script that is most definitly writen weirdly
+def G(A, B, C):
+    print("a"+"b")
+    if A > B:
+        print(C[A-B])
+
+# now this is just some weird code, that will be analized
