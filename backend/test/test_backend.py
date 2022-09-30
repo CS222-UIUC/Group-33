a4b132f063dd2d9b92f9789c070e43a950cf0c39
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-09-30 09:47:31 -0500

    Update and rename test_1.py to test_backend.py
    I am formatting the directories for building tests

---
+++ backend/test/test_backend.py
@@ -0,0 +1,12 @@
+import pytest
+from 
+
+
+
+
+
+# so here wehave to test if the software can run its desiganted functionality
+# first we must test if there is an error
+
+def test_thing():
+    assert True
