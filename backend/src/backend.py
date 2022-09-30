d05a76765d1c35186f133c3126a3d5267e24892d
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-09-29 23:20:30 -0500

    I made a basic server
    I have not tested this yet, but this is how it would look using flask to make a server that responds to the client with an emotion

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -1,11 +1,67 @@
-# https://pypi.org/project/deepface/
-# from deepface import DeepFace
+# ok so here is what our basic flask server will look like
 
-# TODO use the deepface library to do some stuff
+from flask import Flask
+from flask import request
+from deepface import DeepFace
+import tempfile
 
-# here is a python script that is most definitly writen badly
-while True:
-    print("This is bad code")
+backend = Flask(__name__)
 
-print("Does this execute")
-# now this is just some weird code, that will be analized
+# we prevent super large file sizes
+backend.config['MAX_CONTENT_LENGTH'] = 6 * 1000 * 1000
+
+# We use JSON to respond to an API call in this project
+#
+# Generaly we will get a message with
+# {error : True, msg: "<problem with input or server>"}
+# if there is a problem
+
+
+# msg is a string, which contains the error message to be 
+def generate_error(msg):
+    return {
+        error : True,
+        msg : msg
+    }
+
+
+# otherwise we will get a result involving this function
+
+# this takes a deepface result and generates a json output for our app
+def generate_success( result ):
+    return {
+        dominant_emotion: result["dominant_emotion"],
+        emotion: result["emotion"],
+        error: False
+    }
+
+
+# this is our main handle for the server, you call a post request to this 
+@backend.route('/check', methods = ["POST"])
+def check():
+    # we assume the POST method is used based on the API's handler
+    if "file" not in request.files:
+        return generate_error("There was no file in the request")
+    else:
+        file = request.files["file"]
+        
+        result = {}
+        try:
+            result = DeepFace.anaylize(img_path = tempfile.gettempdir() + "/" + file.filename, actions = ['emotion']);
+        except ValueError as err:
+            # TODO (aAccount11) 
+            # perhaps I could make a dumpfile for the instances when this error occures.
+            backend.logger.debug(str(err))
+            return generate_error("An error occured in the processing of the image");
+        except BaseException as err:
+            backend.logger.debug(str(err))
+            return generate_error(f"Unknown Error '{err=}' has occured of type '{type(err)=}'")
+        
+        # this will result in JSON
+        if "dominant_emotion" in result:
+            # this implies that both emotion and dominant emotion are present
+            return generate_success( result )
+        else:
+            return generate_error("Failed understand image")
+        
+        
