00c79a7ecf400acedb323da58fd803580ab58a82
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-09-30 18:28:44 -0500

    Update test_backend.py

--- backend/test/test_backend.py
+++ backend/test/test_backend.py
@@ -1,5 +1,5 @@
 #import pytest
-from backend.main import create_app
+from src.backend.main import create_app
 
 # so here we have to test if the software can run its desiganted functionality
 # first we must test if there is an error
