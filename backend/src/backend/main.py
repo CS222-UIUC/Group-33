2c3d13418b24d5807d96a300dcfb8c0dea577a3b
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-09-30 10:56:50 -0500

    Update main.py

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+# here is our main running function
+def create_app():
+    app = Flask(__name__)
+
+    # we make sure that the users cannot make large files
+    app.config['MAX_CONTENT_LENGTH'] = 6 * 1000 * 1000
+
+    # get our blueprint from backend.py
+    from backend import backend
+    app.register_blueprint(backend)
+
+    return app
