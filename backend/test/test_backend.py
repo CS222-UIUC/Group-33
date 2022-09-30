386c02c0a1f834719a944406bf25b1398575baef
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-09-30 11:17:45 -0500

    Update test_backend.py

--- backend/test/test_backend.py
+++ backend/test/test_backend.py
@@ -1,8 +1,8 @@
-import pytest
-from backend import backend
+#import pytest
+from main import create_app
 
 # so here we have to test if the software can run its desiganted functionality
 # first we must test if there is an error
 
 def test_basic():
-    assert backend
+    assert create_app()
