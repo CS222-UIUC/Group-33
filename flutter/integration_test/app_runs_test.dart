119aa2393a5292b1db051786778f15383c357f1e
Khalid Ali <khalludi123@gmail.com>
2022-09-30 15:43:50 -0500

    Add simple integration test

---
+++ flutter/integration_test/app_runs_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:semaphoreci_flutter_demo/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets(
+      'Should find one instance of main app',
+      (WidgetTester tester) async {
+
+        // Arrange
+        app.main();
+        await tester.pumpAndSettle();
+        final mainAppWidget = find.byKey(const ValueKey('Main App'));
+
+        // Act
+
+
+        // Assert
+        expect(mainAppWidget, findsOneWidget);
+      }
+  );
+
+}
