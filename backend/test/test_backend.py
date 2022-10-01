f9dce9b06b48c3b89a5c9bca236711aa1f60321a
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-09-30 20:04:02 -0500

    Update test_backend.py
    Here I test some stuff to print

--- backend/test/test_backend.py
+++ backend/test/test_backend.py
@@ -5,4 +5,6 @@ from src.backend.main import create_app
 # first we must test if there is an error
 
 def test_basic():
+    print("I fixed the CI now here is a test to see"
+          +" if I can print and load my flask object")
     assert create_app()
