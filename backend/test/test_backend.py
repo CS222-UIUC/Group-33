1462a8a156a24cbd45fa1a4aa2113936f9e418b8
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-09-30 21:14:32 -0500

    Added a legit test
    Here we test if we can get a mood back to the client

--- backend/test/test_backend.py
+++ backend/test/test_backend.py
@@ -5,6 +5,21 @@ from src.backend.main import create_app
 # first we must test if there is an error
 
 def test_basic():
-    print("I fixed the CI now here is a test to see"
-          +" if I can print and load my flask object")
+    # this will create the application, we need to know that works before anything else
     assert create_app()
+
+
+def test_tryImage():
+    # Here we test some sample images (which I got from the public domain)
+    server = create_app()
+
+    client = server.test_client()
+
+    # we should be posting images to the server, and getting results back
+    result = client.post("/check", data={
+        "file":open("images/happy1.jpg","rb")
+    })
+    assert result.json["error"] == False and result.json["dominant_emotion"] == "happy"
+    
+    
+    
