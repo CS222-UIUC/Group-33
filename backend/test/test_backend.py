2c1921fed504b420674bf2aced26314dd7a8e231
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-10-07 14:43:22 -0500

    Update test_backend.py
    Added some more test cases

--- backend/test/test_backend.py
+++ backend/test/test_backend.py
@@ -17,10 +17,32 @@ def test_tryImage():
     client = server.test_client()
 
     # we should be posting images to the server, and getting results back
-    result = client.post("/", data={
+    result = client.post("/check", data={
         "file":open("test/images/happy1.jpg","rb")
     })
     assert result.json["error"] == False and result.json["dominant_emotion"] == "happy"
-    
-    
+
+
+def test_tryBadFiles():
+    # we test what happens when someone forgets to upload a file
+    server = create_app()
+
+    client = server.test_client()
+
+    # we post but fail to upload the file
+    result = client.post("/check", data={})
+    # TODO (aAccount11) make the messages global variables, that we can check against
+    assert result.json["error"] == True and result.json["msg"] == "There was no file in the request"
+
+    # ok well what happens if we load an image with two faces?
+    result = client.post("/check", data={
+        "file":open("test/images/twofaces.jpg","rb")
+    })
+    assert result.json["error"] == True and result.msg["msg"] == "Too many faces"
+
+    # finaly what happens if we load an image with out a face
+    result = client.post("/check", data={
+        "file":open("test/images/unknown.png","rb")
+    })
+    assert result.json["error"] == True and result.json["msg"] == "An error occured in the processing of the image"
     
