9ec572c1d5b95ee828f3e3bbd0a43907b71eed0b
aAccount11 <113577067+aAccount11@users.noreply.github.com>
2022-10-28 04:20:36 -0500

    Add files via upload

---
+++ flutter/lib/localization/en_us_translations.dart
@@ -0,0 +1,22 @@
+final Map<String, String> enUs = {
+  'msg_network_err': 'Network Error',
+  'msg_something_went_wrong': 'Something Went Wrong!',
+  "msg_iphone_13_pro_m2": "iPhone 13 Pro Max - Two",
+  "lbl_login2": "Login",
+  "lbl_welcome_back": "Welcome \nBack!",
+  "msg_don_t_have_an_a": "Don’t have an account? Register!",
+  "msg_check_your_app":
+  "Check your app's UI from the below demo screens of your app.",
+  "msg_let_s_get_start": "Let’s Get\nStarted",
+  "lbl_password": "Password",
+  "msg_login_with_spot": "Login with Spotify",
+  "msg_don_t_have_an_a2": "Don’t have an account?",
+  "msg_forgot_password": "Forgot Password?",
+  "lbl_mood_spot": "Mood\nSpot",
+  "lbl_login": "Login        ->",
+  "lbl_username": "Username",
+  "lbl_register": "Register!",
+  "msg_iphone_13_pro_m": "iPhone 13 Pro Max - One",
+  "msg_the_mood_detect": "The Mood Detecting\n Playlist Generating App",
+  "lbl_app_navigation": "App Navigation"
+};
