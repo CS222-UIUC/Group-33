b9bbf7092b7180bad2c68c911e9e0c4642618fb5
Himnish Jain <67916778+Himnish@users.noreply.github.com>
2022-11-03 14:14:49 -0500

    Add all files

---
+++ flutter/lib/services/spotify_api.dart
@@ -0,0 +1,192 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+import 'package:http_interceptor/http_client_with_interceptor.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+import 'constants/values.dart' as Constants;
+import 'model/playback.dart';
+import 'model/playlist.dart';
+import 'model/track.dart';
+import 'model/user.dart';
+import 'services/api_path.dart';
+import 'services/spotify_interceptor.dart';
+
+typedef NestedApiPathBuilder<T> = String Function(T listItem);
+
+class PremiumRequiredException implements Exception {}
+
+class NoActiveDeviceFoundException implements Exception {}
+
+class SpotifyApi {
+  static Client client = HttpClientWithInterceptor.build(interceptors: [
+    SpotifyInterceptor(),
+  ], retryPolicy: ExpiredTokenRetryPolicy());
+
+  static Future<User> getCurrentUser() async {
+    final response = await client.get(
+      APIPath.getCurrentUser,
+    );
+
+    if (response.statusCode == 200) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw Exception(
+          'Failed to get user with status code ${response.statusCode}');
+    }
+  }
+
+  static Future<User> getUserById(String userId) async {
+    final response = await client.get(
+      APIPath.getUserById(userId),
+    );
+
+    if (response.statusCode == 200) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw Exception(
+          'Failed to get user with status code ${response.statusCode}');
+    }
+  }
+
+  static Future<List<Playlist>> getListOfPlaylists(
+      {int limit = 20, int offset = 0}) async {
+    final response =
+    await client.get(APIPath.getListOfPlaylists(offset, limit));
+
+    if (response.statusCode == 200) {
+      List items = json.decode(response.body)['items'];
+      items = await _expandNestedParam(
+          originalList: items,
+          nestedApiPathBuilder: (item) =>
+              APIPath.getUserById(item['owner']['id']),
+          paramToExpand: 'owner');
+
+      return items.map((item) => Playlist.fromJson(item)).toList();
+    } else {
+      throw Exception(
+          'Failed to get list of playlists with status code ${response.statusCode}');
+    }
+  }
+
+  static Future<Playlist> getPlaylist(String playlistId) async {
+    final response = await client.get(APIPath.getPlaylist(playlistId));
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      final onwerResponse =
+      await client.get(APIPath.getUserById(responseBody['owner']['id']));
+      responseBody['owner'] = json.decode(onwerResponse.body);
+      return Playlist.fromJson(responseBody);
+    } else {
+      throw Exception(
+          'Failed to get a playlist with status code ${response.statusCode}');
+    }
+  }
+
+  static Future<List<Track>> getTracks(String playlistId) async {
+    final response = await client.get(APIPath.getTracks(playlistId));
+
+    if (response.statusCode == 200) {
+      List items = json.decode(response.body)['items'];
+      return items.map((item) => Track.fromJson(item['track'])).toList();
+    } else {
+      throw Exception(
+          'Failed to get tracks. with status code ${response.statusCode}');
+    }
+  }
+
+  static Future<String> getArtistImageUrl(String href) async {
+    final response = await client.get(href);
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      final images = responseBody['images'];
+      if (images.length == 0) return null;
+      for (final image in images) {
+        final imageWidth = image['width'];
+        final imageHeight = image['height'];
+        if (imageWidth < Constants.artistImageMaxSize ||
+            imageHeight < Constants.artistImageMaxSize) {
+          return image['url'];
+        }
+      }
+      return images[0]['url'];
+    } else {
+      throw Exception(
+          'Failed to get artist image url with status code ${response.statusCode}');
+    }
+  }
+
+  static Future<void> play({
+    @required String playlistId,
+    @required String trackId,
+    int positionMs = 0,
+  }) async {
+    final response = await client.put(APIPath.play,
+        body: json.encode({
+          'context_uri': 'spotify:playlist:$playlistId',
+          'offset': {'uri': 'spotify:track:$trackId'},
+          'position_ms': positionMs,
+        }));
+
+    if (response.statusCode == 204) return;
+    if (response.statusCode == 403) {
+      final reason = json.decode(response.body)['error']['reason'];
+      if (reason == 'PREMIUM_REQUIRED ') throw PremiumRequiredException();
+    }
+    if (response.statusCode == 404) throw NoActiveDeviceFoundException();
+  }
+
+  static Future<void> pause() async {
+    final response = await client.put(APIPath.pause);
+    if (response.statusCode == 204)
+      try {
+        final playback = await _getCurrentPlayback();
+        if (!playback.isPlaying)
+          return;
+        else
+          throw Exception();
+      } catch (_) {
+        throw Exception('Failed to pause');
+      }
+
+    if (response.statusCode == 403) {
+      final reason = json.decode(response.body)['error']['reason'];
+      if (reason == 'PREMIUM_REQUIRED ') throw PremiumRequiredException();
+    }
+    if (response.statusCode == 404) throw NoActiveDeviceFoundException();
+  }
+
+  static Future<Playback> _getCurrentPlayback() async {
+    final response = await client.get(APIPath.player);
+    if (response.statusCode == 200) {
+      return Playback.fromJson(json.decode(response.body));
+    } else {
+      throw Exception(
+          'Failed to get player state with status code ${response.statusCode}');
+    }
+  }
+
+  static Stream<Playback> getCurrentPlaybackStream() async* {
+    yield* Stream.periodic(Constants.playerStatePollDuration, (_) async {
+      try {
+        final currentPlayback = await _getCurrentPlayback();
+        return currentPlayback;
+      } catch (_) {
+        return null;
+      }
+    }).asyncMap((event) async => await event);
+  }
+
+  static Future _expandNestedParam(
+      {@required List originalList,
+        @required NestedApiPathBuilder nestedApiPathBuilder,
+        @required String paramToExpand}) async {
+    return Future.wait(originalList
+        .map((item) => client.get(nestedApiPathBuilder(item)).then((response) {
+      item[paramToExpand] = json.decode(response.body);
+      return item;
+    })));
+  }
+}
---
+++ flutter/lib/services/spotify_interceptor.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+import 'model/auth_tokens.dart';
+
+class SpotifyInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    final storage = new FlutterSecureStorage();
+    final accessToken = await storage.read(key: AuthTokens.accessTokenKey);
+    data.headers
+        .addAll({HttpHeaders.authorizationHeader: 'Bearer $accessToken'});
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    return data;
+  }
+}
+
+class ExpiredTokenRetryPolicy extends RetryPolicy {
+  @override
+  Future<bool> shouldAttemptRetryOnResponse(ResponseData response) async {
+    if (response.statusCode == 401) {
+      print("Token expired, updating token...");
+      await AuthTokens.updateTokenToLatest();
+      print("Updated");
+      return true;
+    }
+
+    return false;
+  }
+}
