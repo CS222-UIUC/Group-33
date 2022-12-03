cc20f276c8e53148845919643410c3fcfd6cbbf5
Khalid Ali <khalludi123@gmail.com>
2022-12-02 18:04:09 -0600

    Change path, add image_downloader as widget

--- flutter/lib/TakePictureScreen.dart
+++ flutter/lib/TakePictureScreen.dart
@@ -137,7 +137,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
             var request = http.MultipartRequest('POST', uri)
               ..files.add(await http.MultipartFile.fromPath(
                 'file ', // the label by which you must send the file
-                imageToAPI as String, // the image file, where ever you store that
+                path, // the image file, where ever you store that
               ));
 
             var response = await request.send();
